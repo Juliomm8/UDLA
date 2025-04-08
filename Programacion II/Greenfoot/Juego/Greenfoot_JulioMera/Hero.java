3e269dd3756e4251358a449fe7f31cfb210f891e
Juliomm <juliom2004@hotmail.com>
2025-04-07 20:40:09 -0500

    new

---
+++ Programacion II/Greenfoot/Juego/Greenfoot_JulioMera/Hero.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ *  EL nombre del heroe es "El Botánico Errante".
+ *  Este héroe se desplaza por el mundo y puede
+ *  disparar semillas para eliminar a los enemigos.
+ * 
+ * Controles:
+ * - Flechas del teclado o WASD para moverse
+ * - Barra espaciadora para disparar semillas
+ * 
+ * La clase Hero maneja la lógica de movimiento y disparo.
+ *
+ * @author (Julio Mera) 
+ */
+
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the Hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
