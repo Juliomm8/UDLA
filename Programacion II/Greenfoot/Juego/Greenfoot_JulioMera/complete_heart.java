7ad1edaca9f7e3958de94a9bed2ddcf836c067ce
Juliomm <juliom2004@hotmail.com>
2025-04-09 00:22:12 -0500

    12_090425

---
+++ Programacion II/Greenfoot/Juego/Greenfoot_JulioMera/complete_heart.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class complete_heart here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class complete_heart extends Actor
+{
+    /**
+     * Act - do whatever the complete_heart wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
