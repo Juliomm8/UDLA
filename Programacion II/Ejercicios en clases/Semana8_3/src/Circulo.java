9e358c1117d9ee7d40655101dd3f93b4c62e808b
Juliomm <150650620+Juliomm8@users.noreply.github.com>
2025-05-19 07:52:50 -0500

    new

--- Programacion II/Ejercicios en clases/Semana8_3/src/Circulo.java
+++ Programacion II/Ejercicios en clases/Semana8_3/src/Circulo.java
@@ -1,5 +1,29 @@
-public abstract class Circulo extends Figura{
+public class Circulo extends Figura {
     private int radio;
 
+    public Circulo() {
+        this.radio = (int) (Math.random() * 10 + 1);
+    }
 
+    public Circulo(int radio) {
+        this.radio = radio;
+    }
+
+    public int getRadio() {
+        return radio;
+    }
+
+    public void setRadio(int radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
 }
--- Programacion II/Ejercicios en clases/Semana8_3/src/Rectangulo.java
+++ Programacion II/Ejercicios en clases/Semana8_3/src/Rectangulo.java
@@ -1,4 +1,36 @@
-public abstract class Rectangulo extends Figura{
+
+public class Rectangulo extends Figura {
     private int base;
     private int altura;
+
+    public Rectangulo() {
+        this.base = (int) (Math.random() * 5 + 1);
+        this.altura = (int) (Math.random() * 5 + 1);
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return base * altura;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * (base + altura);
+    }
 }
